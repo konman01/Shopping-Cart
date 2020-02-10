f13e6660cb2e2b9335379223242fc9018c8bef9a
konman01 <konimanjunatha@gmail.com>
2020-02-09 22:22:06 -0800

    Added the middleware

--- app.js
+++ app.js
@@ -12,6 +12,16 @@ const express = require('express');
 
 const app = express();
 
+app.use((req, res, next) => {
+    console.log('In the middleware');
+    next(); // Allows the request to continue to next middleware in line
+});
+
+app.use((req, res, next) => {
+    console.log('In another middleware');
+    res.send('<h1>Hello from the nodejs Server</h1>'); // header like text/html is automatically set usig this utility function
+});
+
 
 const server = http.createServer(app);
 
