dc3a1f15bcf3ceceb36b370a0e3404f9ca8beb5a
konman01 <konimanjunatha@gmail.com>
2020-02-10 21:42:32 -0800

    Added html pages

--- routes/admin.js
+++ routes/admin.js
@@ -1,14 +1,15 @@
 const path = require('path');
 
-
 const express = require('express');
+
+const rootDir = require('../util/path');
 
 const router = express.Router();
 
 // /admin/add-product => GET
 router.get('/add-product', (req, res, next) => {
     // the path add-product goes here, other request g to next
-    res.sendFile(path.join(__dirname, '..', 'views', 'add-product.html'));
+    res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
 });
 
 // /admin/add-product => POST
--- routes/shop.js
+++ routes/shop.js
@@ -2,11 +2,14 @@ const path = require('path');
 
 const express = require('express');
 
+const rootDir = require('../util/path');
+
 const router = express.Router();
 
 
 router.get('/', (req, res, next) => {
-    res.sendFile(path.join(__dirname, '..','views', 'shop.html')); 
+    //res.sendFile(path.join(__dirname, '..','views', 'shop.html')); 
+    res.sendFile(path.join(rootDir,'views', 'shop.html')); 
 });
 // __dirname is the path where we are using it, here it will be /Users/konman01/Desktop/github/shopping-cart
 
