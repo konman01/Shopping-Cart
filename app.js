4afba863e11d4596a5b7016eff42eeb403aaac7c
konman01 <konimanjunatha@gmail.com>
2020-02-12 23:05:25 -0800

    Added includes

--- app.js
+++ app.js
@@ -50,7 +50,7 @@ app.use(shopRoutes);
 
 app.use('/', (req, res, next) => {
     //res.status(404).sendFile(path.join(__dirname, 'views', '404.html')); // sending the html page
-    res.status(404).render('404', {pageTitle: 'Product Not Found'});
+    res.status(404).render('404', {pageTitle: 'Product Not Found', path: ''});
 });
 
 
